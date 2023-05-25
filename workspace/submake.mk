13f5b4a03c63d4ef47e64f42a88509af31e9e008
Bo C. Herlin <bo@idipsum.se>
2023-05-25 12:50:11 +0200

    new era

--- workspace/submake.mk
+++ workspace/submake.mk
@@ -1,12 +1,6 @@
 # assumes ~/texmf points to ../texmf, for example:
 # ln -s ../texmf ~/texmf
 #
-# depends on sagemath, for example:
-# sudo apt-add-repository -y ppa:aims/sagemath
-# sudo apt-get update
-# sudo apt-get install sagemath-upstream-binary
-# sudo cp -R /usr/lib/sagemath/local/share/texmf/tex /usr/local/share/texmf
-# sudo texhash /usr/local/share/texmf
 
 PDF = ../../pdf
 TEXMF = ../texmf
@@ -14,16 +8,17 @@ LAST_COMMENT = ${TEXMF}/tex/latex/last-comment.tex
 CLASS = ${TEXMF}/tex/latex/local/elementary-physics.cls
 BIB = ${TEXMF}/bibtex/bib/local/elementary-physics.bib
 
-.PHONY: all edit pdf read clean test sage tidy
+LATEX = lualatex # pdflatex
+
+.PHONY: all edit pdf read clean test tidy
 
 all pdf : ${PDF}/${FILENAME}.pdf
 
 ${PDF}/${FILENAME}.pdf : ${FILENAME}.tex *.png ${LAST_COMMENT} ${CLASS} ${BIB}
-	pdflatex ${FILENAME}.tex
-	sage ${FILENAME}.sagetex.sage
+	${LATEX} ${FILENAME}.tex
 	bibtex ${FILENAME}.aux
-	pdflatex ${FILENAME}.tex
-	pdflatex ${FILENAME}.tex
+	${LATEX} ${FILENAME}.tex
+	${LATEX} ${FILENAME}.tex
 	mv -f ${FILENAME}.pdf ${PDF}
 
 edit :
@@ -43,23 +38,13 @@ tidy :
 	rm -f ${FILENAME}.synctex.gz
 	rm -f ${FILENAME}.toc
 	rm -f ${FILENAME}-blx.bib
-	rm -f ${FILENAME}.sagetex.*
-	rm -f sage-plots-for-${FILENAME}.tex/*
-	if [ -d sage-plots-for-${FILENAME}.tex ]; then rmdir sage-plots-for-${FILENAME}.tex; fi
 	rm -f *.tex~
 
 clean : tidy
 	rm -f ${PDF}/${FILENAME}.pdf
 
-sage :
-	pdflatex ${FILENAME}.tex
-	sage ${FILENAME}.sagetex.sage
-	pdflatex ${FILENAME}.tex
-	mv -f ${FILENAME}.pdf ${PDF}
-	evince ${PDF}/${FILENAME}.pdf &
-
 test :
-	pdflatex ${FILENAME}.tex
+	${LATEX} ${FILENAME}.tex
 	mv -f ${FILENAME}.pdf ${PDF}
 	evince ${PDF}/${FILENAME}.pdf &
 
